03c1c0e9f11204a3dec20006e1c2a385efd4546f
Double327 <180303665@qq.com>
2022-10-02 23:24:59 +0800

    添加导航

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,16 @@
 module.exports = {
   title: "躺平男孩",
   description: "Just for TangPing!",
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "UI设计", link: "/ui/" },
+      { text: "Web前端", link: "/web/" },
+      { text: "后端", link: "/process/" },
+      { text: "软件工具", link: "/tools/" },
+      { text: "Gitee", link: "https://gitee.com/Double_Float/" },
+      { text: "Github", link: "https://github.com/Double327/" },
+    ],
+  },
 };
--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,13 @@
-# Hello TangPing Boy
-
-[home](/)
-[UI 设计](./ui/)
-[前端](./web/)
-[后端](./process/)
-[工具软件](./tools/)
+---
+home: true
+actionText: 开始内卷 →
+actionLink: /zh/guide/
+features:
+  - title: 躺平至上
+    details: 以躺平为中心
+  - title: 知识驱动
+    details: 享受
+  - title: 高性能
+    details: XXX
+footer: MIT Licensed | Copyright © 2022-present Evan You
+---
