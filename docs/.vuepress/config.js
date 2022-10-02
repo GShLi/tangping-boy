8af7e22a9f4cf2c5ecad8e02b654156952dbd9cc
Double327 <180303665@qq.com>
2022-10-03 00:08:44 +0800

    Web 目录结构

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,8 +6,17 @@ module.exports = {
     nav: [
       { text: "首页", link: "/" },
       { text: "UI设计", link: "/ui/" },
-      { text: "Web前端", link: "/web/" },
+      {
+        text: "Web前端",
+        items: [
+          { text: "HTML", link: "/web/html/" },
+          { text: "CSS", link: "/web/css/" },
+          { text: "JavaScript", link: "/web/js/" },
+          { text: "TypeScript", link: "/web/ts/" },
+        ],
+      },
       { text: "后端", link: "/process/" },
+      { text: "运维", link: "/om/" },
       { text: "软件工具", link: "/tools/" },
       { text: "Gitee", link: "https://gitee.com/Double_Float/" },
       { text: "Github", link: "https://github.com/Double327/" },
